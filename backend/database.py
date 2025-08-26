4a797d10b2da86e47d3b3779fdc3f7e234d7604b
rupam905 <rupamdutta905.com>
2025-08-26 12:39:40 +0530

    added cors and mongo motor

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,9 @@
-from pymongo import MongoClient
+import motor.motor_asyncio
 
-MONGO_URL = "mongodb+srv://rupamdutta905:rupamdutta7980@cluster0.j5ghcds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+MONGO_URL = 'mongodb+srv://rupamdutta905:rupamdutta7980@cluster0.j5ghcds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 
-client = MongoClient(MONGO_URL, tls=True,
-    tlsAllowInvalidCertificates=True)
+client = client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 
 db = client["disease_prediction"]
 users_collection = db["users"]
